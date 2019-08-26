a49db060357570a880200b5e394abc8802e7ecd3
yaoyongchao <yaoyongchao3g@163.com>
2019-08-26 17:55:33 +0800

    1、11

--- app/src/main/java/com/fh/bdc/demo/ListDemoActivity.kt
+++ app/src/main/java/com/fh/bdc/demo/ListDemoActivity.kt
@@ -10,25 +10,27 @@ import com.fh.bdc.R
 import kotlinx.android.synthetic.main.activity_list_demo.*
 import kotlinx.android.synthetic.main.item_demo.view.*
 
+/**
+ * Author: YongChao
+ * Date: 19-8-26 下午4:04
+ * Description: 列表Demo
+ */
+
 class ListDemoActivity : BaseActivity() {
     lateinit var adapter : com.fh.baselib.adapter.BaseAdapter<String>
     var list = mutableListOf<String>()
+    var  s = ""
     override fun layoutId(): Int {
         return R.layout.activity_list_demo
     }
 
     override fun initView() {
-
         initRv()
-
-
     }
 
     private fun initRv() {
         rv.layoutManager = LinearLayoutManager(mContext)
         rv.addItemDecoration(MyDivider(mContext, DividerItemDecoration.VERTICAL))
-
-
         list.add("1")
         list.add("2")
         list.add("3")
@@ -37,6 +39,7 @@ class ListDemoActivity : BaseActivity() {
         list.add("6")
         list.add("7")
         list.add("8")
+        list.add("9")
         adapter = BaseAdapter(R.layout.item_demo,list) { view: View, bean: String,i: Int ->
             view.tv.text = bean
         }
